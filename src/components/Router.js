0b733e2f46d33bdae3c57a22b4e9ad3fc507db44
jioun.lee.dev <jioun.lee@nts-corp.com>
2021-06-23 22:44:23 +0900

    #1 Using Firebase Auth

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({isLoggedIn}) => {
   return (
     <Router>
       <Switch>
